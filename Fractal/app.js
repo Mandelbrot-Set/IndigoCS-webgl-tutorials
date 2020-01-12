2f5c4c9ea093a7c194ea6a4c68dbd145c8deaac8
enimgawxy <1480973357@qq.com>
2020-01-12 10:28:45 +0800

    fractal

--- Fractal/app.js
+++ Fractal/app.js
@@ -46,7 +46,8 @@ function Init() {
 		// fsText:'/fishSwim.glsl'
 		// fsText: '/shader/guil/Mandelflow3.glsl'
 		// fsText: '/shader/guil/Mandelflow2.glsl'
-		fsText: '/shader/guil/Mandeltree.glsl'
+		// fsText: '/shader/guil/Mandeltree.glsl'
+		fsText: '/shader/iq/orbit_traps.glsl'
 		// fsText: '/mandl.fs.glsl'
 	}, loadShaderAsync, RunDemo);
 }
